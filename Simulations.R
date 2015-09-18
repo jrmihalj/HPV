eeb8d9873ffded71979be7c8c701bc1103ca644d
Joseph Mihaljevic <jmihaljevic@uchicago.edu>
2015-09-18 14:48:41 -0500

    updated with all other functions. some preliminary exploration

--- Simulations.R
+++ Simulations.R
@@ -5,8 +5,11 @@
 # HEADER #
 
 source("sim_func.R")
+source("occur_func.R")
+source("null_func.R")
 require(mnormt) #needed for multivariate normal distrib
-require(lme4) #needed for GLMMs
+require(tidyr)
+require(dplyr)
 
 # Required  custom functions:
 Logit <- function(x){
@@ -18,212 +21,86 @@ AntiLogit <- function(x){
 }
 
 ##############################
-# Test (null) Function:
+# Test the functions:
 ##############################
-null <- sim_func()
-head(null)
-#null$Y
+test <- sim_func()
+head(test)
+
+occur_func(test)
+null_func(test, 100)
 
 ##########################################################################################
 ##########################################################################################
 
 # Question 1: 
-# Can hidden within-host correlations bias the estimation of 
-# among-host covariate effects for pathogen strains?
+# How do correlations affect coinfection?
 
-# Simulation 1a - Similar strains, corr in gam
+# Simulation 1a - Similar/different strains, within-host corr in phi and gamma
 # Methods:
-# - Assign no correlation between strains among hosts in any probabilities
-# - Assign strong correlation in within-host gam (i.e. strong priority effects)
-# - Assume no effect of across-time covariates
+# - Assign strong within-host correlation
+# - Assign no among-host correlations
+# - Assume no effect of covariates
 
 
-nsims <- 100
-ints <- mat.or.vec(nr=nsims,nc=2)
-slops <- mat.or.vec(nr=nsims,nc=2)
-pres <- mat.or.vec(nr=nsims,nc=2)
+n.sims <- 50
+out <- list()
 
-for(i in 1:nsims){
-  Sim <- sim_func(n.pat = 100, rwg = 0.8, 
-                  bpat1g = 3, bpat2g = 3)
+for(i in 1:n.sims){
   
-  Sim_null <- sim_func(n.pat = 100, rwg = 0, 
-                       bpat1g = 3, bpat2g = 3)
+  Sim <- sim_func(n.pat = 100, rag = -.9, rap = 0, rwg = 0, rwp = 0, 
+                  bpat1p = 0, bpat2p = 0)
   
-  Simnull.mod <- glmer(Y ~ X.pat.vals + (1|Patient), family= binomial, data = subset(Sim_null, Strain==1))
-  Sim.mod <- glmer(Y ~ X.pat.vals + (1|Patient), family= binomial, data = subset(Sim, Strain==1))
-  
-  ints[i,1] <- Simnull.mod@beta[1]
-  ints[i,2] <- Sim.mod@beta[1]
-  
-  slops[i,1] <- Simnull.mod@beta[2]
-  slops[i,2] <- Sim.mod@beta[2]
-  
-  # Pres counts
-  pres[i,1] <- length(which(subset(Sim_null, Strain==1)$Y == 1))
-  pres[i,2] <- length(which(subset(Sim, Strain==1)$Y == 1))
+  # Run null model analysis:
+  out[[i]] <- null_func(Sim, 1000)
   
 }
-# White = null, Gray = alternative
-hist(ints[,1], breaks=20)
-hist(ints[,2], breaks=20, add=T, col="gray")
 
-hist(slops[,1], breaks=20)
-hist(slops[,2], breaks=20, add=T, col="gray")
-
-hist(pres[,1], breaks=20)
-hist(pres[,2], breaks=20, add=T, col="gray")
-
+# Get the co.dif in a usable format...
+coinf <- sapply(out, "[", 4)
+coinf <- as_data_frame(coinf) # from dplyr
+coinf <- as.vector(t(coinf))
+hist(coinf, breaks=20)
 
 # Observations:
-# - No obvious effect on pres counts, intercepts or slopes. 
+# - Within-host correlations in phi or gamma very rarely affect the number of coinfections.
+# - This makes sense because correlations in psi (occurrence probability), which emerge from strong correlations in phi or gamma,
+#   do not change the AVERAGE occurrence probabilities.
+# - What these correlations would change is the frequency of observing the SAME outcome (pres/abs) for both strains within visits, 
+#   not just co-occurrence.
 
-##############################
+# - However, correlations in phi ACROSS PATIENTS can affect coinfections. 
+#   - Positive correlation can increase coinfection rate, negative correlation tends to decrease rates.
+#   - This is because the correlation at the patient-level changes the average psi for that patient (therefore for all of that
+#     patient's visits). This allows for within-patient co-occurrence to increase on average, even though overall psi for each strain
+#     does not change. 
 
-# Simulation 1b - Different strains, corr in gam
-# Methods:
-# - Assign no correlation between strains among hosts in any probabilities
-# - Assign strong correlation in within-host gam (i.e. strong facilitatory priority effects)
-# - Assume no effect of across-time covariates
-
-
-nsims <- 100
-ints <- mat.or.vec(nr=nsims,nc=2)
-slops <- mat.or.vec(nr=nsims,nc=2)
-pres <- mat.or.vec(nr=nsims,nc=2)
-
-for(i in 1:nsims){
-  Sim <- sim_func(n.pat = 100, rwg = 0.8, 
-                  bpat1g = 0, bpat2g = -5)
-  
-  Sim_null <- sim_func(n.pat = 100, rwg = 0, 
-                       bpat1g = 0, bpat2g = -5)
-  
-  Simnull.mod <- glmer(Y ~ X.pat.vals + (1|Patient), family= binomial, data = subset(Sim_null, Strain==1))
-  Sim.mod <- glmer(Y ~ X.pat.vals + (1|Patient), family= binomial, data = subset(Sim, Strain==1))
-  
-  ints[i,1] <- Simnull.mod@beta[1]
-  ints[i,2] <- Sim.mod@beta[1]
-  
-  slops[i,1] <- Simnull.mod@beta[2]
-  slops[i,2] <- Sim.mod@beta[2]
-  
-  # Pres counts
-  pres[i,1] <- length(which(subset(Sim_null, Strain==1)$Y == 1))
-  pres[i,2] <- length(which(subset(Sim, Strain==1)$Y == 1))
-  
-}
-
-# White = null, Gray = alternative
-hist(ints[,2], breaks=20)
-hist(ints[,1], breaks=20, add=T, col="gray")
-
-hist(slops[,1], breaks=20)
-hist(slops[,2], breaks=20, add=T, col="gray")
-
-hist(pres[,1], breaks=20)
-hist(pres[,2], breaks=20, add=T, col="gray")
-
-
-# Observations:
-# -
-
-
-##############################
-
-# Simulation 1c - Similar strains, corr in phi
-# Methods:
-# - Assign no correlation between strains among hosts in any probabilities
-# - Assign strong positive correlation in within-host phi (i.e. strong facilitatory priority effects)
-# - Assume no effect of across-time covariates
-
-nsims <- 100
-ints <- mat.or.vec(nr=nsims,nc=2)
-slops <- mat.or.vec(nr=nsims,nc=2)
-pres <- mat.or.vec(nr=nsims,nc=2)
-
-for(i in 1:nsims){
-  Sim <- sim_func(n.pat = 100, rwp = 0.8, 
-                  bpat1p = 3, bpat2p = 3)
-  
-  Sim_null <- sim_func(n.pat = 100, rwp = 0, 
-                       bpat1p = 3, bpat2p = 3)
-  
-  Simnull.mod <- glmer(Y ~ X.pat.vals + (1|Patient), family= binomial, data = subset(Sim_null, Strain==1))
-  Sim.mod <- glmer(Y ~ X.pat.vals + (1|Patient), family= binomial, data = subset(Sim, Strain==1))
-  
-  ints[i,1] <- Simnull.mod@beta[1]
-  ints[i,2] <- Sim.mod@beta[1]
-  
-  slops[i,1] <- Simnull.mod@beta[2]
-  slops[i,2] <- Sim.mod@beta[2]
-  
-  # Pres counts
-  pres[i,1] <- length(which(subset(Sim_null, Strain==1)$Y == 1))
-  pres[i,2] <- length(which(subset(Sim, Strain==1)$Y == 1))
-  
-}
-# White = null, Gray = alternative
-hist(ints[,1], breaks=20)
-hist(ints[,2], breaks=20, add=T, col="gray")
-
-hist(slops[,1], breaks=20)
-hist(slops[,2], breaks=20, add=T, col="gray")
-
-hist(pres[,1], breaks=20)
-hist(pres[,2], breaks=20, add=T, col="gray")
+# - Correlations in gamma ACROSS PATIENTS do not seem to affect coinfections...
+#   - Need to think about why
 
 ##########################################################################################
 ##########################################################################################
 
 # Question 2: 
-# Can among-host correlations bias the estimation of 
-# among-host covariate effects for pathogen strains?
+# How do among-host correlations affect coinfection?
 
-# Simulation 2a - Similar strains, corr in gam
+# Simulation 1a - Similar/different strains, corr in phi (persistence)
 # Methods:
-# - Assign no correlation between strains among hosts in any probabilities
-# - Assign strong correlation in among-host gam (i.e. strong priority effects)
-# - Assume no effect of across-time covariates
+# - Assign no among-host correlations
+# - Assign strong within-host correlation in phi (i.e. strong competition/facilitation)
+# - Assume no effect of covariates
 
 
-nsims <- 100
-ints <- mat.or.vec(nr=nsims,nc=2)
-slops <- mat.or.vec(nr=nsims,nc=2)
-pres <- mat.or.vec(nr=nsims,nc=2)
+n.sims <- 1
+out <- list()
 
-for(i in 1:nsims){
-  Sim <- sim_func(n.pat = 100, rag = 0.8, 
-                  bpat1g = 3, bpat2g = 3)
+for(i in 1:n.sims){
   
-  Sim_null <- sim_func(n.pat = 100, rag = 0, 
-                       bpat1g = 3, bpat2g = 3)
+  Sim <- sim_func(n.pat = 100, rag = 0.9, rap = 0, rwg = 0.9, rwp = 0, 
+                  bpat1p = 0, bpat2p = 0)
   
-  Simnull.mod <- glmer(Y ~ X.pat.vals + (1|Patient), family= binomial, data = subset(Sim_null, Strain==1))
-  Sim.mod <- glmer(Y ~ X.pat.vals + (1|Patient), family= binomial, data = subset(Sim, Strain==1))
-  
-  ints[i,1] <- Simnull.mod@beta[1]
-  ints[i,2] <- Sim.mod@beta[1]
-  
-  slops[i,1] <- Simnull.mod@beta[2]
-  slops[i,2] <- Sim.mod@beta[2]
-  
-  # Pres counts
-  pres[i,1] <- length(which(subset(Sim_null, Strain==1)$Y == 1))
-  pres[i,2] <- length(which(subset(Sim, Strain==1)$Y == 1))
+  # Run null model analysis:
+  out[[i]] <- null_func(Sim, 1000)
   
 }
-# White = null, Gray = alternative
-hist(ints[,1], breaks=20)
-hist(ints[,2], breaks=20, add=T, col="gray")
 
-hist(slops[,1], breaks=20)
-hist(slops[,2], breaks=20, add=T, col="gray")
-
-hist(pres[,1], breaks=20)
-hist(pres[,2], breaks=20, add=T, col="gray")
-
-
-# Observations:
-# - No obvious effect on pres counts, intercepts or slopes. 
 
