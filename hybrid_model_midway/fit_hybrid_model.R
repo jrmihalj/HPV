0b1f7e103df451f0985346d4fb290733038741e5
Sylvia Lise Ranjeva <slr@midway-login1.rcc.local>
2016-03-17 10:22:10 -0500

    save summary with Rhat

--- hybrid_model_midway/fit_hybrid_model.R
+++ hybrid_model_midway/fit_hybrid_model.R
@@ -13,39 +13,47 @@ n_site <- c(20,40,60,80,100,150,200,300,400,500)
 version_corr <- c(1,2,3)
 
 sweep_df <- data.frame( n_time = rep(n_timesteps, each = length(n_site)), n_s = rep(n_site, each = length(version_corr)), version = rep(version_corr, times = length(n_timesteps)*length(n_site)))
+sweep_df <- sweep_df[sweep_df$version == 1,]
 n_timesteps <- sweep_df[k,]$n_time
 n_site <- sweep_df[k,]$n_s
 Rp_filename <- paste0("Rp_",sweep_df[k,]$version,".csv" )
 Ro_filename <- paste0("Ro_",sweep_df[k,]$version,".csv" )
-
+dat_filename <- paste0("full_dat_",sweep_df[k,]$version,".rda")
+load(dat_filename)
 
 # locations/patients
 n <- n_timesteps * n_site # number of observations
 
 
-source('sim_data_metah.R')
+#source('sim_data_metah.R')
 
-stan_d <- list(n_unit = n_site, 
+stan_d_realization <- list(n_unit = n_site, 
                n_time = n_timesteps,
                n = n, 
                m = m,
-               unit = site,
-               time = time,
-               y = y,
-               y_mat = y)
-m_init <- NULL
-m_init <- stan('metah.stan', data=stan_d, iter=10, chains=1)
+               unit = stan_d$unit[stan_d$unit %in% c(1:n_site) & stan_d$time %in% c(1:n_timesteps)],
+               time = stan_d$time[stan_d$unit %in% c(1:n_site) & stan_d$time %in% c(1:n_timesteps)],
+               y = stan_d$y[stan_d$unit %in% c(1:n_site) & stan_d$time %in% c(1:n_timesteps),],
+               y_mat = stan_d$y_mat[stan_d$unit %in% c(1:n_site) & stan_d$time %in% c(1:n_timesteps),]
+               )
 
-m_fit <- stan(fit = m_init, data=stan_d, warmup=100, iter=2000, chains=2, cores=2, 
+m_init <- NULL
+m_init <- stan('metah.stan', data=stan_d_realization, iter=10, chains=1)
+
+m_fit <- stan(fit = m_init, data=stan_d_realization, warmup=100, iter=2000, chains=4, cores=4, 
               pars=c('R_p', 'R_o', "beta_phi", "beta_gam"),
               control=list(adapt_delta=0.9))
+
+summary <- as.data.frame(summary(m_fit)$summary)
+summary$trial <- k
+
 
 #traceplot(m_fit, pars=c('lp__'))
 
 post <- extract(m_fit)[1:4]
 post_dfs <- lapply(post,as.data.frame)
 for( i in 1:length(post_dfs)){
-  post_dfs[[i]]$trial <- k
+ post_dfs[[i]]$trial <- k
 }
 tableNames <- names(post)
 
@@ -55,6 +63,7 @@ for( i in 1:length(tableNames)){
   tableName <- tableNames[[i]]
   dbWriteTable(db,tableName,df, append=TRUE)
 }
+dbWriteTable(db,"summary",summary, append=TRUE)
 dbDisconnect(db)
 
 # # evaluate recovery of R_p
