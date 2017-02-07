85c439a1a6b4a98b86506b57c2f4e35d0fef7834
Joseph Mihaljevic <jmihaljevic@uchicago.edu>
2017-02-07 11:13:19 -0600

    minor changes for Gardner

--- code/R/fit_model_tarbell.R
+++ code/R/fit_model_tarbell.R
@@ -82,8 +82,23 @@ log_lik = extract(m_fit, pars = "log_lik")$log_lik
 # Make it an observation x sample data frame
 log_lik = t(log_lik)
 log_lik_df = as.data.frame(log_lik)
-#log_lik_df$chain = k
-table_name <- "loglik"
+
+n_col_max = 200
+col_indices <- c(1:ncol(log_lik_df))
+col_splits <- split(col_indices, ceiling(seq_along(col_indices)/n_col_max))
+
 db <- dbConnect(SQLite(), dbResultsFilename)
-dbWriteTable(db,table_name,log_lik_df, append=TRUE)
+for( i in c(1:length(col_splits))){
+  table_name <- paste0("loglik_",i)
+  ind <- unlist(col_splits[[i]]) 
+  log_lik_df_subset <- log_lik_df[,ind]
+  dbWriteTable(db,table_name,log_lik_df_subset, append=TRUE)
+  print(paste("Done with split", i))
+}
 dbDisconnect(db)
+
+# #log_lik_df$chain = k
+# table_name <- "loglik"
+# db <- dbConnect(SQLite(), dbResultsFilename)
+# dbWriteTable(db,table_name,log_lik_df, append=TRUE)
+# dbDisconnect(db)
