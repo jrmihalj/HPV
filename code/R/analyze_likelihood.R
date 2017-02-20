f85fede938c7d597c6c847d9f4ebad9bc6aa9580
Joseph Mihaljevic <jmihaljevic@uchicago.edu>
2017-02-20 13:51:55 -0600

    likelihood analysis, etc.

--- code/R/analyze_likelihood.R
+++ code/R/analyze_likelihood.R
@@ -7,7 +7,7 @@ library(loo)
 
 setwd("./output/")
 
-files <- list.files(pattern = "fit_null_nofixed")
+files <- list.files(pattern = "norand_chain")
 ll_all <- data.frame()
 for( i in 1:length(files)){
   dbResultsFilename <- files[i]
@@ -36,26 +36,55 @@ for( i in 1:length(files)){
   dbDisconnect(db)
 }
 
-# WAIC function needs an S x N matrix
+# WAIC and LOO-IC functions need an S x N matrix
 # S = number posterior draws; N = number of data points/observations
-waic_nofixed <- waic(t(as.matrix(ll_all)))
-save(waic_nofixed, file = "waic_null_nofixed.rda")
+
+# WAIC
+waic_norand <- waic(t(as.matrix(ll_all)))
+save(waic_norand, file = "waic_norand.rda")
+
+# LOO-IC
+looic_norand = loo(t(as.matrix(ll_all)))
+save(looic_norand, file = "looic_norand.rda")
+
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+
+# Summarize WAIC
 
 ## Once all Lhoods have been calculated: ### 
 waic_files <- list.files(pattern = "waic")
 #waic_all <- data.frame()
 for( i in 1:length(waic_files)){
   load(waic_files[i])
-  #identifier <- strsplit(strsplit(waic_files[i],"[_]")[[1]][3],"[.]")[[1]][1]
-  #df <- data.frame(waic = waic$waic, se = waic$se_waic, model = identifier)
-  #waic_all <- rbind(waic_all, df)
 }
 
 waic_tab =
-  rbind(sapply(list(waic_null, waic_norand, waic_nofixed), function(x) round(x$waic,2)),
-      sapply(list(waic_null, waic_norand, waic_nofixed), function(x) round(x$se_waic,2)))
+  rbind(sapply(list(waic_null, waic_norand, waic_nofixed, waic_full), function(x) round(x$waic,2)),
+      sapply(list(waic_null, waic_norand, waic_nofixed, waic_full), function(x) round(x$se_waic,2)))
 
-colnames(waic_tab) = c("Null", "NoRand", "NoFixed")
+colnames(waic_tab) = c("Null", "NoRand", "NoFixed", "Full")
 rownames(waic_tab) = c("WAIC", "SE_WAIC")
 
-waic_tab
+t(waic_tab)
+
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+
+# Summarize LOO-IC
+
+## Once all Lhoods have been calculated: ### 
+loo_files <- list.files(pattern = "loo")
+#loo_all <- data.frame()
+for( i in 1:length(loo_files)){
+  load(loo_files[i])
+}
+
+loo_tab =
+  rbind(sapply(list(looic_null, looic_norand, looic_nofixed, looic_full), function(x) round(x$looic,2)),
+        sapply(list(looic_null, looic_norand, looic_nofixed, looic_full), function(x) round(x$se_looic,2)))
+
+colnames(loo_tab) = c("Null", "NoRand", "NoFixed", "Full")
+rownames(loo_tab) = c("loo-ic", "SE_loo-ic")
+
+t(loo_tab)
